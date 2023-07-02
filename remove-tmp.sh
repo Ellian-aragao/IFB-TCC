6eacd0309e56ff4f5db8600393d0cf2304e2e445
Ellian Aragão Dias <ellian.aragao@gmail.com>
2023-07-02 12:54:04 -0300

    feat: script to delete tmp files

---
+++ remove-tmp.sh
@@ -0,0 +1,1 @@
+rm -f ifbtcc.aux ifbtcc.bbl ifbtcc.fdb_latexmk ifbtcc.fls ifbtcc.log ifbtcc.pdf ifbtcc.toc ifbtcc.blg ifbtcc.out ifbtcc.synctex.gz text/conclusao.aux text/conclusion.aux text/desenv.aux text/desenvolvimento.aux text/introducao.aux text/introduction.aux text/objetivo.aux appendix/mapping-study.aux
